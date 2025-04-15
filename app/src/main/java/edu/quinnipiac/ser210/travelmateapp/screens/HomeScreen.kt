7fd1e5928d67342ac4d5fe112ff39b1dd1be6ea6
Reeya-Patel <reeyapatel1010@gmail.com>
2025-04-14 21:46:02 -0400

    bigger buttons

--- app/src/main/java/edu/quinnipiac/ser210/travelmateapp/screens/HomeScreen.kt
+++ app/src/main/java/edu/quinnipiac/ser210/travelmateapp/screens/HomeScreen.kt
@@ -3,8 +3,10 @@ package edu.quinnipiac.ser210.travelmateapp.screens
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import edu.quinnipiac.ser210.travelmateapp.navigation.Screens
 
@@ -13,27 +15,36 @@ fun HomeScreen(navController: NavController) {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(24.dp),
-        verticalArrangement = Arrangement.Center
+            .padding(32.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Button(
-            onClick = {
-                navController.navigate(Screens.BrowseCountriesScreen.name)
-            },
-            modifier = Modifier.fillMaxWidth()
+            onClick = { navController.navigate(Screens.BrowseCountriesScreen.name) },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp),
+            shape = MaterialTheme.shapes.large
         ) {
-            Text("Browse Countries")
+            Text(
+                text = "Browse Countries",
+                fontSize = 30.sp
+            )
         }
 
-        Spacer(modifier = Modifier.height(16.dp))
+        Spacer(modifier = Modifier.height(24.dp))
 
         Button(
-            onClick = {
-                navController.navigate(Screens.PlanningScreen.name)
-            },
-            modifier = Modifier.fillMaxWidth()
+            onClick = { navController.navigate(Screens.PlanningScreen.name) },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp),
+            shape = MaterialTheme.shapes.medium
         ) {
-            Text("Plan Your Trip")
+            Text(
+                text = "Plan Your Trip",
+                fontSize = 30.sp
+            )
         }
     }
 }
